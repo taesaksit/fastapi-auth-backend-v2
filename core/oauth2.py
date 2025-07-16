685980c615b8a7b1e3d33777f72ab69e7807bcc6
taepc <taesaksit09@gmail.com>
2025-07-17 02:16:26 +0700

    update format code

--- core/oauth2.py
+++ core/oauth2.py
@@ -9,14 +9,17 @@ from core.security import SECRET_KEY, ALGORITHM
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         print("payload is ", payload)
@@ -27,7 +30,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
 
     user = get_user_by_email(db, email)
-    
+
     #  เหตุผลที่ต้องเช็ค if user is None
     # 1.Token อาจเป็นของ user ที่ถูกลบไปแล้ว
     if user is None:
